1df64fb8b5157eb10ebb06263e7544b78acfa403
Luke Edwards <luke@silverstripe.com>
2018-04-19 14:50:04 +1200

    Reapply changes

--- src/RestfulServer.php
+++ src/RestfulServer.php
@@ -38,6 +38,8 @@ use SilverStripe\CMS\Model\SiteTree;
  * @todo i18n integration (e.g. Page/1.xml?lang=de_DE)
  * @todo Access to extendable methods/relations like SiteTree/1/Versions or SiteTree/1/Version/22
  * @todo Respect $api_access array notation in search contexts
+ * @todo Make $allowed_sort_params a configurable option
+ * @todo Make $allowed_dir_params a configurable option
  */
 class RestfulServer extends Controller
 {
@@ -72,6 +74,21 @@ class RestfulServer extends Controller
      * @var string
      */
     protected static $default_mimetype = "text/xml";
+
+    /**
+     * Sort params that are acceptable for filtering.
+     *
+     * @var array
+     */
+    protected static $allowed_sort_params = array('Created', 'PublishDate', 'Sort');
+
+    /**
+     * Dir params that are acceptable for filtering.
+     *
+     * @var array
+     */
+    protected static $allowed_dir_params = array('asc', 'desc');
+
 
     /**
      * @uses authenticate()
@@ -310,6 +327,43 @@ class RestfulServer extends Controller
 
         return $responseFormatter->convertDataObject($obj, $fields);
     }
+
+    /**
+     * Ensures that the url param passed for the sort param is valid.
+     * This is to prevent sql injection.
+     *
+     * @return array|user_error|null
+     */
+    protected function getSortQuery()
+    {
+        $sortQuery = $this->request->getVar('sort');
+        if (in_array($sortQuery, self::$allowed_sort_params)) {
+            return $sortQuery;
+        } elseif ($sortQuery) {
+            user_error('Disallowed Sort parameter', E_USER_ERROR);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Ensures that the url param passed for the dir param is valid.
+     * This is to prevent sql injection.
+     *
+     * @return array|user_error|null
+     */
+    protected function getDirQuery()
+    {
+        $dirQuery = $this->request->getVar('dir');
+        if (in_array(strtolower($dirQuery), self::$allowed_dir_params)) {
+            return $dirQuery;
+        } elseif ($dirQuery) {
+            user_error('Disallowed Dir parameter', E_USER_ERROR);
+        } else {
+            return null;
+        }
+    }
+
 
     /**
      * Uses the default {@link SearchContext} specified through
